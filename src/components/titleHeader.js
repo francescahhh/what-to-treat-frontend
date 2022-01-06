c89ff3134e840330570d2b3122371abd896011a6
Francesca Hamm <francesca.c.hamm@gmail.com>
2022-01-06 03:53:03 -0500

    added text

--- src/components/titleHeader.js
+++ src/components/titleHeader.js
@@ -3,7 +3,8 @@ import React from "react"
 const TitleHeader = () => {
 return (
     <header className="Main-header">
-        <h1>What to TrEAT ?</h1>
+        <h1 id="page-title">what to trEAT?</h1>
+        <h5 id="pinata-click"> Click the piñata!</h5>
     </header>
 )
 }
