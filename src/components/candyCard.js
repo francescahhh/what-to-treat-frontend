44317d576aa8228ca909787de56283c176ba13cc
Francesca Hamm <francesca.c.hamm@gmail.com>
2022-01-06 14:29:19 -0500

    added notes

--- src/components/candyCard.js
+++ src/components/candyCard.js
@@ -1,1 +1,1 @@
-//hooked up to the ruby file here somehow to display the data
+//hooked up to the ruby file here somehow to display the data--display name of restaurant and category (does not need to display user's name)
